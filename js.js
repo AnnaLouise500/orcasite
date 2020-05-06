58ee6c150fd1adef24705d2b33bd5e44c624d294
Anna <annamorrison500@hotmail.com>
2020-05-06 17:38:46 +0100

    modal complete for raffle choices
    modal for raffle 1 complete on iphone 5
    demo for owners

--- js.js
+++ js.js
@@ -1,11 +1,30 @@
-var item = document.querySelector('.logo-o');
+//Get modal element
+var modal = document.getElementById('modal');
+//Get open modal button
+var modalBtn = document.getElementById('modalBtn');
+//Get close button
+var close = document.getElementById('close');
 
-item.addEventListener('load', (e) => {
-    item.classList.toggle('invisible');
-    item.addEventListener('animationend', animationEndCallback);
-});
+//Listen for open click
+modalBtn.addEventListener('click', openModal);
+//Listen for close click
+close.addEventListener('click', closeModal);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
 
-animationEndCallback = (e) => {
-    item.removeEventListener('animationend', animationEndCallback);
-    item.classList.remove('invisible');
+//Function to open modal
+function openModal(){
+    modal.style.display = "block";
+}
+//Function to close modal
+function closeModal(){
+    modal.style.display = "none";
+}
+
+//Function to close modal if outside click
+function outsideClick(e){
+    if(e.target == modal) {
+        modal.style.display = 'none'
+    }
+
 }
